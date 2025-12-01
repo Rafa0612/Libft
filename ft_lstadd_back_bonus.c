7347a4db2356c8bfe5e9be8f6887987def866589
Rafael Pena Rodriguez <rpena-ro@c3r7s6.42malaga.com>
2025-12-01 18:55:02 +0100

    hay que terminar el lstmap, hacer el makefile y añadirlos a la lib

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,12 +6,11 @@
 /*   By: rpena-ro <rpena-ro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 17:35:43 by rpena-ro          #+#    #+#             */
-/*   Updated: 2025/12/01 17:51:13 by rpena-ro         ###   ########.fr       */
+/*   Updated: 2025/12/01 18:54:43 by rpena-ro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "ft_lstlast_bonus.c"
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
